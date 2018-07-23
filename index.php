798da797d720db846e5598b1df61c6e06da1c4ba
Gregory Arnal <gregory.arnal@hotmail.com>
2018-07-23 22:55:44 +0200

    testE

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php SESSION_START();?>
+<!DOCTYPE html>
+<html>
+<title>Photo</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="css/stylecss.css">
+
+<body>
+
+<!-- Navbar (sit on top) -->
+
+<?php include 'includes/header.php'; ?>
+
+<div class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+  <img class="w3-image" src="image/IMG_0883.JPG" alt="Architecture" width="1500" height="750"></img>
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>GA</b></span> <span class="w3-hide-small w3-text-light-grey">Photo</span></h1>
+  </div>
+</div>
+
+
+
+  </body>
+</html>
